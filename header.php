d45fafe770a3c2748286a91f4550795a8bdf58de
Alan Chao <a@acd.gs>
2014-09-25 11:53:08 -0400

    oops, corrected commented out tracking

--- header.php
+++ header.php
@@ -11,15 +11,15 @@
         <?php wp_head(); ?>
         <script type="text/javascript" src="http://fast.fonts.net/jsapi/dd228574-4dab-4330-8c69-17a6a414a03f.js"></script>
         <script type="text/javascript">
-            // var _gaq = _gaq || [];
-            // _gaq.push(['_setAccount', 'UA-24286242-2']);
-            // _gaq.push(['_trackPageview']);
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-24286242-2']);
+            _gaq.push(['_trackPageview']);
         
-            // (function() {
-            // var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-            // ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            // var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-            // })();
+            (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
         </script>
     </head>
     <body>
