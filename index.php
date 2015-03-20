08e02cd731e414d11c3caf753d12a9a969e20e1a
Alan Chao <a@acd.gs>
2015-03-20 09:29:32 -0400

    removed home page content, edited footer states

--- index.php
+++ index.php
@@ -38,52 +38,6 @@ if( get_field('home_featured') ): ?>
         </div>
     </div>
 <?php endif; ?>
-
-    <aside class="home_gist">
-        <div class="gist_inner">
-            <h3>Welcome to <br /> Piscatello Design Centre</h3>
-            <p>We partner with our clients to set the vision and strategy required to design and deliver brand experiences that truly engage their audiences.</p>
-        </div>
-    </aside>
-
-    <section class="goto">
-        <article>
-            <a href="<?php echo get_permalink( 5 ); ?>">
-                <div class="swapper">
-                    <img src="<?php bloginfo('stylesheet_directory'); ?>/images/helen.jpg">
-                    <img src="<?php bloginfo('stylesheet_directory'); ?>/images/nt_lightbox.jpg">
-                </div>
-                <div class="label vertical_center_parent">
-                    <p class="vertical_center">Projects</p>
-                    <div class="divhelper"></div>
-                </div>
-            </a>
-        </article>
-        <article>
-            <a href="<?php echo get_permalink( 7 ); ?>">
-                <div class="swapper">
-                    <img src="<?php bloginfo('stylesheet_directory'); ?>/images/studio.jpg">
-                    <img src="<?php bloginfo('stylesheet_directory'); ?>/images/studio_eames.jpg">
-                </div>
-                <div class="label vertical_center_parent">
-                    <p class="vertical_center">Studio</p>
-                    <div class="divhelper"></div>
-                </div>
-            </a>
-        </article>
-    </section>
-    <aside class="quote">
-        <div>
-            <?php
-                $rows = get_field('quotes' ); // get all the rows
-                $rand_row = $rows[ array_rand( $rows ) ]; // get a random row
-                $rand_row_quote = $rand_row['quote_text' ]; // get the sub field value 
-                $rand_row_attribution = $rand_row['quote_attribution' ]; // get the sub field value 
-            ?>
-            <p class="the_quote"><?php echo $rand_row_quote; ?></p>
-            <p class="attr">&mdash;<?php echo $rand_row_attribution; ?></p>
-        </div>
-    </aside>
     <div class="clients_wrapper graybg">
         <aside class="clients">
             <?php $i = 0; while( $i < 16 ) : ?>
