915120ea54f7bd028aa98b68edb83e2c4f0e91ea
Alan Chao <a@acd.gs>
2015-03-31 15:28:53 -0400

    changed arrow to load when cover loads

--- js/single-project.js
+++ js/single-project.js
@@ -23,7 +23,7 @@ jQuery(document).ready(function($) {
             $(this).animate({opacity: 1}, 600, "easeOutSine");
         }, {offset: '95%'});
     });
-    $(window).load(function(){
+    $(".cover_image").load(function(){
         $(".more_arrow i").removeClass("hidden").addClass("visible");
     });
     
