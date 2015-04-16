d826da44b5cd1e1ab778cfc656c02f8ce8485a3a
Alan Chao <a@acd.gs>
2015-04-16 10:35:26 -0400

    more arrow js

--- js/studio.js
+++ js/studio.js
@@ -1,6 +1,5 @@
 jQuery(document).ready(function($) {
 
-    var about_image = $(".about_image");
     var studio_more_arrow = $(".studio_more_arrow i");
 
     $(window).scroll(function(){
@@ -9,24 +8,30 @@ jQuery(document).ready(function($) {
         } else {
             $(".header_container").addClass("clearbg").removeClass("solidbg");
         }
+        if( $("body").scrollTop() + $(window).height() > $(".about_image").height() ) {
+            studio_more_arrow.removeClass("visible").addClass("hidden");
+        } else {
+            studio_more_arrow.removeClass("hidden").addClass("visible");
+        }
     });
 
     studio_more_arrow.click(function(){{
         $('html, body').animate({
             scrollTop: $(".profile_section").offset().top
         }, 1000, "easeInOutQuint");
-        studio_more_arrow.removeClass("visible").addClass("hidden");
     }});
 
     function studio_page_down(){
-        if( about_image.height() > $(window).height() ) {
+        if( $(".about_image").height() > $(window).height() ) {
             studio_more_arrow.removeClass("hidden").addClass("visible");
         } else {
             studio_more_arrow.removeClass("visible").addClass("hidden");
         }
     }
-    studio_page_down();
+
     $(window).resize(function(){
         studio_page_down();
     });
+
+    studio_page_down();
 });
